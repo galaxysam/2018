e723b230bd2b4e3421e7858c83385310dc27f88e
galaxysam <735239379@qq.com>
2019-06-18 00:28:17 +0800

    c-

--- table1.cpp
+++ table1.cpp
@@ -4,32 +4,31 @@ class Table {
 public:
 
   void show();
-  int addRow();
+  void addRow();
   void delRow(int n);
-  int addColumn();
+  void addColumn();
   void delColumn(int n);
+  void xin(int a,int b);
+  void set(int a,int b,char d[7]);
 private:
-  Cell cells;
+  char c[10][10][7];
+  int row;
+  int col;
 };
-class Cell {
-public:
-  Cell(char con[]);
-  ~Cell();
 
-  void show();
-private:
-  string content;
-};
 int main() {
   Table tb;
+  tb.xin(0,0);
   tb.show();
   tb.addRow();
   tb.show();
   tb.addColumn();
   tb.show();
-  Table tb1(5,5);
+  Table tb1;
+  tb1.xin(5,5);
   tb1.show();
-  tb1.set(1,1,30);
+  tb1.set(1,1,"30");
+  tb1.show();
   tb1.set(2,2,"hello");
   tb1.show();
   tb1.delRow(1);
@@ -37,4 +36,64 @@ int main() {
   tb1.delColumn(1);
   tb1.show();
   return 0;
+}
+
+
+
+void Table::xin(int a,int b)
+{
+	row=a;
+	col=b;
+	for(int i=0;i<10;i++)
+	{
+		for(int j=0;j<10;j++)
+		{
+			for(int k=0;k<7;k++)
+			{
+				c[i][j][k]=' ';
+			}
+		}
+	}
+}
+
+void Table::addRow()
+{
+	row++;
+}
+void Table::addColumn()
+{
+	col++;
+}
+void Table::delColumn(int n)
+{
+	col=col-n;
+}
+void Table::delRow(int n)
+{
+	row=row-n;
+}
+void Table::show()
+{
+	for(int i=0;i<row;i++)
+	{
+		for(int j=0;j<col;j++)
+		{
+			for(int k=0;k<7;k++)
+			{
+				std::cout<<c[i][j][k];
+			}
+			std::cout<<"|||";
+		}
+		std::cout<<std::endl;std::cout<<std::endl;
+	}
+	std::cout<<std::endl;std::cout<<std::endl;std::cout<<std::endl;
+}
+
+void Table::set(int a,int b,char d[7])
+{
+	for(int i=0;i<7;i++)
+	{
+	
+	c[a][b][i]=d[i];
+	}
 }
